1da814db6a0fbda64feac379d156c6beebf8b6cb
yamatch <matchooooka@outlook.com>
2017-09-12 16:37:13 +0900

    ルーティングを一つにまとめた

--- responce.py
+++ responce.py
@@ -9,14 +9,9 @@ from bottle import request,response,template,redirect
 from bottle import route,HTTPError
 from bottle import get,post,run
 
-# サーバを立ち上げ
-if __name__ =='__main__':
-    run(host="localhost", port=8080,
-        debug=True, reloader=True)
-
 # クライアントから音声ファイルを受け取る
 @route('/upload', method='POST')
-def do_upload():
+def upload():
     upload = request.files.get('upload')
     name, ext = os.path.splitext(upload.filename)
 
@@ -24,28 +19,30 @@ def do_upload():
     return template('{{string}} is uploaded.\n', string = upload.filename) # check
 
 
-# クライアントから送られた音声ファイルをHoundifyに認識させる
-'''
-(⊃|⊂) → (⊃*⊂)
-ケツマンおっぴろげてメールを待つ
-'''
+    # クライアントから送られた音声ファイルをHoundifyに認識させる
+    '''
+    (⊃|⊂) → (⊃*⊂)
+    ケツマンおっぴろげてメールを待つ
+    '''
 
-# houndifyからのJSONをダンプ
-result = '{"otintin":"biroooon","samurai":"shakiiin","tintinzamurai":"tintinzamurai" }'
-# houndifyから送信されたJSONをresultに代入する予定
+    # houndifyからのJSONをダンプ
+    result = '{"otintin":"biroooon","samurai":"shakiiin","tintinzamurai":"tintinzamurai" }'
+    # houndifyから送信されたJSONをresultに代入する予定
 
-dec = json.dumps(result)
+    dec = json.loads(result)
 
-res_data = {"Result":dec["otintin"],"song":dec["samurai"],"artist":dec["tintinzamurai"],"album":dec["tintinzamurai"]}
-# クライアントにJSON形式で結果を送信
-res = json.dumps(res_data)
+    res_data = {"Result":dec["otintin"],"song":dec["samurai"],"artist":dec["tintinzamurai"],"album":dec["tintinzamurai"]}
 
-@route('/resp')
-def resp():
+
+    # クライアントにJSON形式で結果を送信
+
+    res = json.dumps(res_data)
     response.status = 200
     response.content_type = "application/json"
     return res
 
-# print(res_data)
 
-
+# サーバを立ち上げ
+if __name__ =='__main__':
+    run(host="localhost", port=8080,
+        debug=True, reloader=True)
